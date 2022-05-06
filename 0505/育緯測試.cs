872a88a942665577f601d246ecc16daf95fe2cf6
jason79619@gmail.com <jason79619@gmail.com>
2022-05-06 15:05:22 +0800

    0506.育緯

---
+++ 0505/育緯測試.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0505
+{
+    public partial class 育緯測試 : Form
+    {
+        public 育緯測試()
+        {
+            InitializeComponent();
+        }
+    }
+}
