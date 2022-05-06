122c43c843b0c3c38ac9d6800828e944ebc7d31b
慶陽 劉 <wsxqaz664422@gmail.com>
2022-05-06 15:44:29 +0800

    05.06.慶陽測試

---
+++ 0505/慶陽測試.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0505
+{
+    public partial class 慶陽測試 : Form
+    {
+        public 慶陽測試()
+        {
+            InitializeComponent();
+        }
+    }
+}
